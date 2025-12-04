1dce23be7c04851707e0a963019f34b0d1c2b0ff
AquilaXk <illusiveman7@gmail.com>
2025-12-04 15:45:16 +0900

    work

---
+++ src/main/java/com/back/P141041Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P141041Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P141041Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/P141041ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P141041ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
