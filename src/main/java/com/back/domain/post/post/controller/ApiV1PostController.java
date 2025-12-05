b97338d66639bf5ba73ca873b3bce75e6aca1bad
AquilaXk <illusiveman7@gmail.com>
2025-12-05 14:13:16 +0900

    step ~12

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -5,6 +5,7 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +20,16 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
         return items.stream()
-                .map(post -> new PostDto(post))
+                .map(PostDto::new)
                 .toList();
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostDto getItem(@PathVariable int id) {
         Post post = postService.findById(id).get();
         return new PostDto(post);
